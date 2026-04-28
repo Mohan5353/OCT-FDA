e6a5678ad602599165ce3a128bf24b907a281f3d
Mohan5353 <n200125@rguktn.ac.in>
2026-04-28 17:14:06 +0530

    feat: add even smaller models (ResNet-10, MobileNetV2, ConvNeXt-T/A, TinyUnet) and resume benchmark

--- src/train_custom_models.py
+++ src/train_custom_models.py
@@ -145,13 +145,15 @@ def main(args):
     elif args.model_name == "resnet18":
         base_model = smp.Unet(encoder_name="resnet18", encoder_weights="imagenet", in_channels=3, classes=4)
     elif args.model_name == "resnet10":
-        base_model = smp.Unet(encoder_name="tu-resnet10", encoder_weights="imagenet", in_channels=3, classes=4)
+        base_model = smp.Unet(encoder_name="tu-resnet10t", encoder_weights="imagenet", in_channels=3, classes=4)
     elif args.model_name == "mobilenet_v2":
         base_model = smp.Unet(encoder_name="mobilenet_v2", encoder_weights="imagenet", in_channels=3, classes=4)
     elif args.model_name == "convnext_large":
         base_model = smp.Unet(encoder_name="tu-convnext_large", encoder_weights="imagenet", in_channels=3, classes=4)
     elif args.model_name == "convnext_tiny":
         base_model = smp.Unet(encoder_name="tu-convnext_tiny", encoder_weights="imagenet", in_channels=3, classes=4)
+    elif args.model_name == "convnext_atto":
+        base_model = smp.Unet(encoder_name="tu-convnext_atto", encoder_weights="imagenet", in_channels=3, classes=4)
     else: raise ValueError("Unknown model name")
 
     model = CustomFlexibleUDAModel(base_model, mode=args.mode, fda_L=args.fda_L).to(device)
@@ -286,7 +288,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_name", type=str, required=True, choices=["anamnet", "segresnet", "missformer", "resnet101", "resnet50", "resnet18", "resnet10", "mobilenet_v2", "convnext_large", "convnext_tiny", "tiny_unet"])
+    parser.add_argument("--model_name", type=str, required=True, choices=["anamnet", "segresnet", "missformer", "resnet101", "resnet50", "resnet18", "resnet10", "mobilenet_v2", "convnext_large", "convnext_tiny", "convnext_atto", "tiny_unet"])
     parser.add_argument("--mode", type=str, default="baseline", choices=["baseline", "fda", "ms-fda", "adv-fda", "adv-1to1", "ddsp", "dann"])
     parser.add_argument("--data_root", type=str, default="data")
     parser.add_argument("--batch_size", type=int, default=4)
